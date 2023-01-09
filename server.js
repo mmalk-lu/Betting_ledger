9257e419dc2a927e681712fd09f06b1121b52248
Faiz Malkani <malkanifaiz@gmail.com>
2023-01-10 02:04:53 +0530

    Replaced table

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const shortId = require('shortid')
 const ShortUrl = require('./models/shortUrl')
 const cors = require('cors')
 const app = express()
 app.use(cors())
 
-mongoose.connect('mongodb://localhost/urlShortener', {
-  useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect('mongodb://localhost/linkShortener', {
+    useNewUrlParser: true, useUnifiedTopology: true
 })
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
+    const shortUrls = await ShortUrl.find()
+    var generatedUrl = req.query.generated
+    res.render('index', { shortUrls: shortUrls, generated: generatedUrl })
 })
 
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
 
-  res.redirect('/')
+    const shortUrl = shortId.generate()
+    await ShortUrl.create({ full: req.body.fullUrl, short:shortUrl })
+
+    res.redirect('/?generated=' + shortUrl)
 })
 
 app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
 
-  shortUrl.clicks++
-  shortUrl.save()
+    shortUrl.clicks++
+    shortUrl.save()
 
-  res.redirect(shortUrl.full)
+    res.redirect(shortUrl.full)
 })
+
+
 
 app.listen(process.env.PORT || 5000);
